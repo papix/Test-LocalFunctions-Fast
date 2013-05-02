0df76ce596d0e5c76d89e78b12875c1e656b9b53
papix <mail@papix.net>
2013-05-02 20:07:34 +0900

    initial commit

---
+++ lib/Test/LocalFunctions/Fast.pm
@@ -0,0 +1,39 @@
+package Test::LocalFunctions::Fast;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::LocalFunctions::Fast - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::LocalFunctions::Fast;
+
+=head1 DESCRIPTION
+
+Test::LocalFunctions::Fast is ...
+
+=head1 LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+papix E<lt>mail@papix.netE<gt>
+
+=cut
+
